869242189103e30191c52bc0f7245e5961dd0d97
ray <ray@programacionextrema.es>
2019-08-08 19:16:18 +0200

    Carga inicial

---
+++ chat.php
@@ -0,0 +1,18 @@
+<html>
+   <head></head>
+   <body >
+<?php
+
+
+include_once("funciones.inc");
+//cogerOAuth();
+//info();
+cogerChats();
+echo "<br/>";
+cogerStream();
+echo "<br/>";
+
+cogerChat();
+?>   
+   </body>
+</html>	
